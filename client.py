0acc931ef23bb115f93586f0ddab4f97dded431d
Bruno FS <bruno.serbena@gmail.com>
2018-05-28 16:35:00 -0300

    Added server thread
    Signed-off-by: Bruno FS <bruno.serbena@gmail.com>

--- client.py
+++ client.py
@@ -1,17 +1,18 @@
 #!python
 import socket
 
+PORT = 14699
+BUFSIZ = 1024
+
 # create socket
 sock = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM)
 # get local name
 host = socket.gethostname()
 # connect to host on the port.
-port = 14699
-sock.connect((host, port))
+sock.connect((host, PORT))
 # Receive
-msgBytesMax = 1024
-msg = sock.recv(msgBytesMax)
+msg = sock.recv(BUFSIZ)
 # end connection
 sock.close()
 print(msg.decode('ascii'))
--- server.py
+++ server.py
@@ -1,24 +1,84 @@
 #!python
+
 import socket
+import threading
+import sys
 
-# create socket
-serverSocket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
-# get local name
-host = socket.gethostname()
-# bind to the port
-port = 14699
-serverSocket.bind((host, port))
-# queue up requests
-queueSize = 5
-serverSocket.listen(queueSize)
+BUFSIZ = 8192
+g_ThreadTCP = None
+g_Host = ""
 
-while True:
-    # establish connection
-    clientSocket, addr = serverSocket.accept()
-    print("%s connected." % str(addr))
-    # send response
-    msg = 'Connected to ' + host + "\r\n"
-    clientSocket.send(msg.encode('ascii'))
-    # end connection
-    clientSocket.close()
+
+class ServerTCP(threading.Thread):
+    PORT = 14699
+    # Communication state constants
+    REQUESTED = 0
+    OK = 1
+    NOK = 2
+    ABORT = 3
+
+    def __init__(self, serverId):
+        threading.Thread.__init__(self)
+        # Construction parameters
+        self.Id = serverId
+        print("Creating server #%d" % self.Id)
+        sys.stdout.flush()
+
+        print("Creating TCP socket port:%d." % ServerTCP.PORT)
+        sys.stdout.flush()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Connection dictionary: [host] = connection
+        self.connDic = {}
+
+        self.queueSize = 5
+
+    # Thread method invoked when started
+    def run(self):
+        global g_Host
+
+        print("ServerTCP running ID: %s"
+              % str(threading.current_thread().ident))
+        sys.stdout.flush()
+
+        print("Bind TCP %s:%d" % (g_Host, ServerTCP.PORT))
+        sys.stdout.flush()
+        self.socket.bind((g_Host, ServerTCP.PORT));
+
+        print("Server listening...")
+        sys.stdout.flush()
+        self.socket.listen(self.queueSize)
+
+        while True:
+            try:
+                # establish connection
+                conn, addr = self.socket.accept()
+                print("Connected %s:%d" % (str(addr[0]), addr[1]))
+                sys.stdout.flush()
+                self.connDic[str(addr[0])] = conn
+
+                # send response
+                msg = 'Connected to ' + g_Host + ":" + str(self.PORT) + "\r\n"
+                conn.send(msg.encode('ascii'))
+                # end connection
+                conn.close()
+                del self.connDic[str(addr[0])]
+            except (KeyboardInterrupt, SystemExit):
+                for connection in self.connDic:
+                    connection.close()
+                break
+
+
+def main(argv):
+    global g_Host
+    global g_ThreadTCP
+
+    g_Host = socket.gethostbyname(socket.getfqdn())
+
+    serverId = 3
+    g_ThreadTCP = ServerTCP(serverId)
+    g_ThreadTCP.start()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
