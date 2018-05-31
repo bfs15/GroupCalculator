1a9b1d8c3379e9ea9a78e9da1e1ac8c5fce60059
Bruno FS <bruno.serbena@gmail.com>
2018-05-31 01:27:29 -0300

    client now TCP/UDP overloaded
    Signed-off-by: Bruno FS <bruno.serbena@gmail.com>

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 import remotes
 import sys
 import select
+from abc import abstractmethod
 
 import logger
 
@@ -10,10 +11,13 @@ import logger
 BUFSIZ = 8192
 TIMEOUT = 4
 
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
 
 # Creates a non-blocking TCP socket file descriptor
 def create_socket():
-    g_ClientLog.print("[Client] Created socket")
+    g_ClientLog.print("[ClientTCP] Created socket")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(0)
     return sock
@@ -35,98 +39,122 @@ def connect_server(remote):
         # Tries to establish connection
         sock.connect_ex((host, port))
     except Exception as e:  # Other exception
-        g_ClientLog.print("[Client] Exception: " + str(e) + " on connect server %s:%d" % (host, port))
+        g_ClientLog.print("[ClientTCP] Exception: " + str(e) + " on connect server %s:%d" % (host, port))
 
     # return the non-blocking socket attempting connection
     return sock
 
 
-def main_tcp():
-    # Create Loggers
-    global g_ClientLog
-    # serverLogFile = open("Client.log", "w")
-    # g_ClientLog = logger.Logger(serverLogFile)
-    g_ClientLog = logger.Logger(sys.stdout)
-    g_ClientLog.header("Client")
+class Client:
+    @abstractmethod
+    def send_exp(self, expression):
+        pass
 
-    # remote_list returns a array of tuples (hostname, port) from servers.txt
-    remote_list, _ = remotes.create_remote_list()
+    @abstractmethod
+    def receive_result(self):
+        pass
 
-    while True:
-        # recebe a expressão aritmética do usuário.
-        expression = input("Type an arithmetic expression. Example: 1+1, (13+1)*2, 5^3\n")
-        received_result = False
+    @abstractmethod
+    def close(self):
+        pass
+
+
+class ClientTCP(Client):
+    def __init__(self, remote_list):
+        self.remote_list = remote_list
+        self.ready_to_write = []
+        self.socks = []
+
+    def send_exp(self, expression):
         # para cada endereço local do remote_list, iremos iterar.
         # tenta conectar no servidor. Se for um sucesso, ele envia a expressão
         # e aguarda o recebimento da resposta. Caso contrário, procede para a
         # próxima iteração.
-        socks = []
-        for idx, remote in enumerate(remote_list):
-            g_ClientLog.print("[Client] Requesting server #%d" % idx)
+        self.socks = []
+        for idx, remote in enumerate(self.remote_list):
+            g_ClientLog.print("[ClientTCP] Requesting server #%d" % idx)
             # tenta conectar no servidor da iteracao atual
             # non blocking
             sock = connect_server(remote)
-            socks.append(sock)
+            self.socks.append(sock)
 
-        g_ClientLog.print("[Client] Waiting any server for %ds..." % TIMEOUT)
+        g_ClientLog.print("[ClientTCP] Waiting any server for %ds..." % TIMEOUT)
         # this will block until at least one socket is ready || Timeout
-        _, ready_to_write, in_error = select.select([], socks, [], TIMEOUT)
+        print(self.socks)
+        sys.stdout.flush()
+        _, self.ready_to_write, in_error = select.select([], self.socks, [], TIMEOUT)
         # if not timeout
-        if ready_to_write:
+        if self.ready_to_write:
             # for all those that connected
-            for sock in ready_to_write:
-                g_ClientLog.print("[Client] Connected to " + str(sock.getsockname()))
-                g_ClientLog.print("[Client] Sending expression " + expression)
+            for sock in self.ready_to_write:
+                g_ClientLog.print("[ClientTCP] Connected to " + str(sock.getsockname()))
+                g_ClientLog.print("[ClientTCP] Sending expression: " + expression)
                 # mensagem é codificada em ascii
                 msg = expression.encode('ascii')
                 # mensagem é enviada ao servidor da iteracao atual
                 sock.send(msg)  # This is will not block
 
-            g_ClientLog.print("[Client] Waiting any server result")
-            # this will block until at least one socket is ready
-            # wait response from one of the connections, select only from those you wrote
-            ready_to_read, _, in_error = select.select(ready_to_write, [], [], TIMEOUT)
+    def receive_result(self):
+        g_ClientLog.print("[ClientTCP] Waiting any server result")
+        # this will block until at least one socket is ready
+        # wait response from one of the connections, select only from those you wrote
+        ready_to_read, _, in_error = select.select(self.ready_to_write, [], [], TIMEOUT)
+        # only the leader should be ready
+        for sock in ready_to_read:
+            result = sock.recv(BUFSIZ).decode('ascii')
+            g_ClientLog.print("[ClientTCP] Receiving from " + str(sock.getsockname()))
+            return result
 
-            # only the leader should be ready
-            for sock in ready_to_read:
-                try:
-                    # mensagem é recebida com a resolução da primeira
-                    sock.setblocking(1)
-                    g_ClientLog.print("[Client] Receiving from " + str(sock.getsockname()))
-                    result = sock.recv(BUFSIZ).decode('ascii')
-                    received_result = True
-                    # print expression result received from server
-                    if result == "exception":
-                        print("[Client] An exception was detected. Try a valid mathematical expression")
-                    elif result == "zero division":
-                        print("[Client] A division by zero was detected. Try a valid mathematical expression")
-                    else:
-                        print("result = " + result)
-                    sys.stdout.flush()
-                except socket.timeout:
-                    g_ClientLog.print("[Client] Server received expression but didn't respond")
-                    print("Server timeout, try again")
-                    sys.stdout.flush()
-                    pass
-                except Exception as e:  # Other exception
-                    g_ClientLog.print("[Client] Exception: " + str(e))
-
-        for sock in socks:
-            g_ClientLog.print("[Client] Closing socket " + str(sock.getsockname()))
+    def close(self):
+        for sock in self.socks:
+            g_ClientLog.print("[ClientTCP] Closing socket " + str(sock.getsockname()))
             sock.close()
 
-        if not received_result:
-            g_ClientLog.print("[Client] No server could connect")
-            print("All servers down")
-            sys.stdout.flush()
+
+class ClientUDP(Client):
+    def __init__(self):
+        self.sock = None
+
+    def send_exp(self, expression):
+        g_ClientLog.print("[ClientUDP] Creating socket")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.sock.settimeout(TIMEOUT)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+        # sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        g_ClientLog.print("[ClientUDP] Multicasting "+str((MCAST_GRP, MCAST_PORT))+" expression: " + expression)
+        data = expression.encode('ascii')
+        self.sock.sendto(data, (MCAST_GRP, MCAST_PORT))
+
+    def receive_result(self):
+        g_ClientLog.print("[ClientUDP] Waiting any server for %ds..." % TIMEOUT)
+        data, addr = self.sock.recvfrom(BUFSIZ)
+        g_ClientLog.print("[ClientUDP] Received from " + str(addr))
+        result = data.decode('ascii')
+        return result
+
+    def close(self):
+        self.sock.close()
 
 
-# TCP code
-MCAST_GRP = '224.1.1.1'
-MCAST_PORT = 5007
+def main(argv):
+    global client
 
+    def err():
+        print("Usage: %s [TCP/UDP]" % argv[0])
+        sys.exit()
 
-def main_udp():
+    if len(sys.argv) == 2:
+        if sys.argv[1].lower() == "tcp":
+            # remote_list returns a array of tuples (hostname, port) from servers.txt
+            remote_list, _ = remotes.create_remote_list()
+            client = ClientTCP(remote_list)
+        elif sys.argv[1].lower() == "udp":
+            client = ClientUDP()
+        else:
+            err()
+    else:
+        err()
+
     # Create Loggers
     global g_ClientLog
     # serverLogFile = open("Client.log", "w")
@@ -138,16 +166,13 @@ def main_udp():
         # recebe a expressão aritmética do usuário.
         expression = input("Type an arithmetic expression. Example: 1+1, (13+1)*2, 5^3\n")
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        sock.settimeout(TIMEOUT)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-        # sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-        data = expression.encode('ascii')
-        sock.sendto(data, (MCAST_GRP, MCAST_PORT))
-        try:
-            data, addr = sock.recvfrom(1024)
-            result = data.decode('ascii')
+        client.send_exp(expression)
 
+        received_result = False
+        try:
+            result = client.receive_result()
+            received_result = True
+            # print expression result received from server
             if result == "exception":
                 print("[Client] An exception was detected. Try a valid mathematical expression")
             elif result == "zero division":
@@ -155,23 +180,21 @@ def main_udp():
             else:
                 print("result = " + result)
             sys.stdout.flush()
-            print('from ' + str(addr))
         except socket.timeout:
-            g_ClientLog.print("[Client] No server responded")
+            g_ClientLog.print("[Client] Timeout")
             print("Server timeout, try again")
             sys.stdout.flush()
-        g_ClientLog.print("[ClientUDP] Closing socket " + str(sock.getsockname()))
-        sock.close()
+            pass
+        except Exception as e:  # Other exception
+            g_ClientLog.print("[Client] Exception: " + str(e))
+
+        if not received_result:
+            g_ClientLog.print("[Client] No server could respond")
+            print("No response")
+            sys.stdout.flush()
+
+        client.close()
 
 
 if __name__ == "__main__":
-    if sys.argv.__len__() < 2:
-        print("Wrong parameters! ./python3 client [TCP/UDP]")
-        exit()
-    if sys.argv[1].lower() == "tcp":
-        main_tcp()
-    elif sys.argv[1].lower() == "udp":
-        main_udp()
-    else:
-        print("Wrong parameters! ./python3 client [TCP/UDP]")
-        exit()
+    main(sys.argv[0:])
